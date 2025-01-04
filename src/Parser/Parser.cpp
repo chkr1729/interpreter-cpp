b3a4232a68dae6e792e2ed33dde2c9e08b5faca7
Chandrasekhar Karnam <Chandu@MacBook-Pro-3.local>
2025-01-04 17:04:04 -0500

    Adding paranthesis parsing functionality

--- src/Parser/Parser.cpp
+++ src/Parser/Parser.cpp
@@ -17,13 +17,24 @@ void Parser::parse()
             case TokenType::Whitespace:
             case TokenType::Comment:
             case TokenType::MultiCharToken:
+                break;
             case TokenType::SingleCharToken:
+                if (token.getLexeme() == "(")
+                {
+                    std::cout << "(group ";
+                    index++;
+                    parse();
+                }
+                else if (token.getLexeme() == ")")
+                {
+                    std::cout << ")";
+                }
                 break;
             case TokenType::StringLiteral:
-                std::cout << tokens[index].getLiteral() << std::endl;
+                std::cout << tokens[index].getLiteral();
                 break;
             case TokenType::NumberLiteral:
-                std::cout << token.getLiteral() << std::endl;
+                std::cout << token.getLiteral();
                 break;
             case TokenType::Identifier:
                 break;
@@ -31,7 +42,7 @@ void Parser::parse()
                 const std::string lexeme = token.getLexeme();
                 if (isBooleanLiteral(lexeme))
                 {
-                    std::cout << lexeme << std::endl;
+                    std::cout << lexeme;
                 }
                 break;
             }
